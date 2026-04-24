fac2c1b30556f84492dbed1bc3082f6be1863180
e3507967-sys <e3507967@gmail.com>
2026-04-24 18:10:36 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -81,7 +81,7 @@ def update_file():
 
     header = (
         "#profile-title: ⚪WHITELIST ⚪\n"
-        "#announce: Данная подписка собирается из других, просьба не путать с авторскими!!
+        "#announce: Данная подписка собирается из других, просьба не путать с авторскими!!\n
         "#profile-update-interval: 1\n\n"
     )
     
